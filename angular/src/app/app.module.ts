60f4dc4e676c8c2f9a559a09fffeffd09fd1ee2c
albertovzmr <crashban_vazquez2@hotmail.com>
2019-12-10 21:04:42 -0600

    Update app.module.ts
    Nothing to add.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { UsersComponent } from './users/users.component';
+import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     UsersComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    EditUserComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
