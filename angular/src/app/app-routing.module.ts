590aa4f16724b0556e6941a275bcaf0768be336c
albertovzmr <crashban_vazquez2@hotmail.com>
2019-12-10 20:37:36 -0600

    Update routing module.ts
    Nada que agregar.

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { RegisterComponent } from './register/register.component';
 
+const routes: Routes = [
+  {path: 'home', component: UsersComponent},
+	{path: 'login', component: LoginComponent},
+  {path: 'crear', component: RegisterComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
