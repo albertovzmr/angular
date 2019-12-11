a4d81da0bd1f633980851880b66e718ef3a2f66c
albertovzmr <crashban_vazquez2@hotmail.com>
2019-12-10 20:47:00 -0600

    Finishing login component
    Nada que agregar

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { HttpClient } from 'selenium-webdriver/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+	public user: User;
+	public status:string;
+	public identity;
+	public token;
+  public _http: HttpClient;
+  input;
+
+  constructor(
+		private _route: ActivatedRoute,
+		private _router: Router,
+		private apiService: ApiService
+		){
+		this.user = new User("","","","","","","","");
+
+
+	}
 
   ngOnInit() {
+    this.input = {
+      username: '',
+      password: ''
+    };
   }
 
+  login(){
+		// logear al usuario y conseguir sus datos
+		this.apiService.login(this.input).subscribe(
+			response => {
+        console.log(response);
+				
+          if(response.key){
+          
+          // conseguir token
+          this.token = response.key;
+          localStorage.setItem('token',this.token);
+          this._router.navigate(['/home']);
+				}
+				
+			},
+			error => {
+				var errorMessage = <any>error;
+				if (errorMessage != null) {
+					this.status = 'error';
+				}
+			})
+	}
+
 }
+
+
