97de72ca366d1153bdfc8a1fe2ded4b84576b0e5
albertovzmr <crashban_vazquez2@hotmail.com>
2019-12-10 20:55:51 -0600

    Updating register component
    Nothing to add.

--- angular/src/app/register/register.component.ts
+++ angular/src/app/register/register.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/services/api.service';
+import { User } from 'src/models/user';
+import { HttpClient } from 'selenium-webdriver/http';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+	selector: 'register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+	public title: string;
+	public user: User;
+	public status: string;
+	public identity;
+	public token;
+	public _http: HttpClient;
+	public carreras;
 
-  ngOnInit() {
-  }
+
+
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router,
+		private apiService: ApiService
+	) {
+		this.user = new User("", "", "", "", "", "", "", "false");
+
+
+	}
+
+	ngOnInit() {
+		this.apiService.getCarreras().subscribe(response => {
+			console.log(response);
+			this.carreras = response;
+		})
+		console.log(this.carreras);
+	}
+
+
+
+	setCarrera(carrera) {
+		console.log(carrera);
+		this.user.carrera = carrera;
+	}
+	createUser() {
+		this.apiService.create(this.user).subscribe(
+			response => {
+				console.log(response);
+				this._router.navigate(['/home']);
+
+			},
+			error => {
+				console.log(<any>error);
+			}
+		);
+	}
+
+	volver() {
+		this._router.navigate(['/home']);
+	}
+
 
 }
